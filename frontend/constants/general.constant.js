a345c6c7e4e56dfecdd20056e67253d65403fe4e
ssmarika <smarikashrestha02@gmail.com>
2025-04-28 13:56:59 +0545

    Register Form

---
+++ frontend/constants/general.constant.js
@@ -0,0 +1,16 @@
+export const roles = [
+  {
+    id: 1,
+    role: "admin",
+  },
+  { id: 2, role: "client" },
+];
+
+export const genders = [
+  {
+    id: 1,
+    gender: "male",
+  },
+  { id: 2, gender: "female" },
+  { id: 3, gender: "others" },
+];
--- frontend/validation-schema/register.validation.schema.js
+++ frontend/validation-schema/register.validation.schema.js
@@ -1,10 +1,14 @@
 import * as Yup from "yup";
 
 export const registerValidationSchema = Yup.object({
-  email: Yup.string().required().trim().max(55).lowercase().email(),
-  password: Yup.string().required().trim(),
-  firstName: Yup.string().required().trim().max(30),
-  lastName: Yup.string().required().trim().max(30),
-  gender: Yup.string().required().oneOf(["male", "female", "other"]),
-  role: Yup.string().required().oneOf(["admin", "client"]),
+  firstName: Yup.string().required("First Name is required"),
+  lastName: Yup.string().required("Last Name is required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+  gender: Yup.string().required("Gender is required"),
+  role: Yup.string().required("Role is required"),
 });
